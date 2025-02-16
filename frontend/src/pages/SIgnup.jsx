a11979c3bb0ff79e25124ac47157496a16d8a8fd
Zillion <highprechi@gmail.com>
2025-02-16 03:58:23 +0000

    pr

--- frontend/src/pages/SIgnup.jsx
+++ frontend/src/pages/SIgnup.jsx
@@ -11,8 +11,12 @@ export default function Signup() {
     e.preventDefault();
   
     try {
-      const response = await axios.post("https://back-one-navy.vercel.app/auth/signup", { email, password });
+      const response = await axios.post("https://back-one-navy.vercel.app/auth/signup", {userName, email, password }, data, {
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'no-cors', // Disable CORS (but limits the response)
+      });
       alert("User registered successfully!");
+
       console.log("Response:", response.data);
     } catch (error) {
       console.error("Signup Error:", error);
