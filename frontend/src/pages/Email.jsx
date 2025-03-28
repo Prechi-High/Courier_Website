7c0ee4d79e297d76bc2d06d758d12d4e7077e369
Zillion <highprechi@gmail.com>
2025-03-28 14:56:55 +0000

    email

--- frontend/src/pages/Email.jsx
+++ frontend/src/pages/Email.jsx
@@ -6,7 +6,10 @@ import React, { useState } from "react";
 const EmailSender = () => {
     const [email, setEmail] = useState("");
     const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
+    const [trackingNumber, setTrackingNumber] = useState("");
+    const [amount, setAmount] = useAmount("");
+    const [dueDate, setDueDate] = useDueDate("");
+   
     const [status, setStatus] = useState(null);
 
     // Function to handle sending email
@@ -15,7 +18,7 @@ const EmailSender = () => {
             const response = await fetch("https://back-one-navy.vercel.app/auth/send-email", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ to: email, subject, message })
+                body: JSON.stringify({ to: email, subject, trackingNumber, amount,dueDate})
             });
             
             const data = await response.json();
@@ -30,7 +33,8 @@ const EmailSender = () => {
             <h2>Send an Email</h2>
             <input type="email" placeholder="Recipient Email" value={email} onChange={e => setEmail(e.target.value)} />
             <input type="text" placeholder="Subject" value={subject} onChange={e => setSubject(e.target.value)} />
-            <textarea placeholder="Message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+            <input type="email" placeholder="Amount" value={amount} onChange={e => setAmount(e.target.value)} />
+            <input type="text" placeholder="Due date" value={dueDate} onChange={e => setDueDate(e.target.value)} />
             <button onClick={sendEmail}>Send Email</button>
             {status && <p>{status}</p>}
         </div>
