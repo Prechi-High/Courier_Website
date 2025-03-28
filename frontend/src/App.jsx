e0008db88a2bc015b04ed7b88a226217a1737522
Zillion <highprechi@gmail.com>
2025-03-28 10:39:33 +0000

    email

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Track from "./pages/Track";
-import Email from "./pages/Email"
+import <EmailSender></EmailSender> from "./pages/Email"
 import AdminPage from "./pages/AdminPage";
 import Signup from "./pages/SIgnup";
 import Home from "./pages/Home";
